50ac70f00dc7b50e5d7b01a6dce1724e02eb067c
akihiro takamura <takamura@sbsnt.co.jp>
2016-02-22 20:23:24 +0900

    modefied oauth setting

--- src/main/java/springboot/oauth/sample/config/SecurityConfig.java
+++ src/main/java/springboot/oauth/sample/config/SecurityConfig.java
@@ -1,18 +1,26 @@
 package springboot.oauth.sample.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter.XFrameOptionsMode;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableOAuth2Sso
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  @Value("${security.oauth2.custom.server-logout-url}") private String serverLogoutUrl;
+  
+  @Value("${security.oauth2.custom.server-logouted-redirect-url}") private String serverLogoutedRedirectUrl;
+  
   @Override
   public void configure(HttpSecurity http) throws Exception {
     http
@@ -26,9 +34,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       .antMatchers("/admin/**").hasRole("ADMIN")
       .anyRequest().authenticated()
       .and()
-      .logout().logoutUrl("/logout").permitAll()
-      .logoutSuccessUrl("/")
+      .csrf()
+        .csrfTokenRepository(csrfTokenRepository())
+      .and()
+      .logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .logoutSuccessUrl(serverLogoutUrl + "?next=" + serverLogoutedRedirectUrl)
+        .deleteCookies("JSESSIONID")
+        .invalidateHttpSession(true)
+        .permitAll()
     ;
   }
-    
+ 
+  private CsrfTokenRepository csrfTokenRepository() {
+    HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+    repository.setHeaderName("X-XSRF-TOKEN");
+    return repository;
+  }
 }
